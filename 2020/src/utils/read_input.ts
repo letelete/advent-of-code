cb90f578cbc1a6c20db016ea6223ee7d37035bee
Bruno Kawka <brunokawka@gmail.com>
2020-12-09 00:55:17 +0100

    Remove unmandatory details

--- 2020/src/utils/read_input.ts
+++ 2020/src/utils/read_input.ts
@@ -3,7 +3,7 @@ import config from '../config';
 import readFromFile from './read_file';
 import {readSolutionDay} from './read_arguments';
 
-const readInput = (strategy: ParsingStrategy): string | null => {
+const readInput = (strategy: ParsingStrategy): string => {
   const day = readSolutionDay();
   const filePath = config.inputDirectoryPath(day);
   const rawInput = readFromFile(filePath);
