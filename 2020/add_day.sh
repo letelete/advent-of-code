1f4c6f6ed05372eea0f33d946490ba5de8fc2e41
Bruno Kawka <brunokawka@gmail.com>
2020-12-11 00:25:49 +0100

    Add 0-prefixed numeration for better days sorting

--- 2020/add_day.sh
+++ 2020/add_day.sh
@@ -2,7 +2,8 @@
 
 DAY=$1
 
-DAY_DIR_PATH="./src/days/day_$1"
+DAY_DIR_PATH=$(sh ./get_day_dirname.sh $DAY)
+
 [[ -d $DAY_DIR_PATH ]] && echo "Direcotry <$DAY_DIR_PATH> already exists. Aborting..." && exit 0
 
 mkdir $DAY_DIR_PATH
@@ -10,32 +11,49 @@ cd $DAY_DIR_PATH
 
 # Create a solution.ts file
 echo "import Day from '../../interfaces/day.interface';
-import readInput from '../../utils/read_input';
+import DayInput from '../../interfaces/day_input.interface';
 
-const day$DAY = (): Day => {
-const data: string = readInput(str => 'test' /* TODO: Parse the input */);
+const day$DAY: Day = (input: DayInput) => {
+  const data = input; /* TODO: Parse the input */
 
-// TODO: Implement part one
-const partOne = () => {
-return '';
-};
+  // TODO: Implement part one
+  const partOne = () => {
+    return '';
+  };
 
-// TODO: Implement part two
-const partTwo = () => {
-return '';
-};
+  // TODO: Implement part two
+  const partTwo = () => {
+    return '';
+  };
 
-return {solutions: [partOne, partTwo]};
+  return [partOne, partTwo];
 };
 
 export default day$DAY;
 " >solution.ts
 
+# Create a test file
+echo "import day from './solution';
+
+describe('First sample', () => {
+  const input = '';
+  const [partOne, partTwo] = day(input);
+
+  describe('Part one', () => {
+    it('should return NUMBER with a sample input', () => {
+      expect(partOne()).toEqual(NUMBER);
+    });
+  });
+
+  describe('Part two', () => {
+    it('should return DIFFERENTNUMBER with a sample input', () => {
+      expect(partTwo()).toEqual(DIFFERENTNUMBER);
+    });
+  });
+});" >solution.test.ts
+
 # Create an input file
 echo >input
-
-# Create a test file
-echo >solution.test.ts
 
 echo "Happy Coding! 🎄🎅"
 
--- 2020/src/config.ts
+++ 2020/src/config.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 
 const resolveFilePathAtDayDir = (day: number, fileName: string) => {
-  return path.resolve(__dirname, `../src/days/day_${day}/${fileName}`);
+  const dayNumber: string = day < 10 ? `0${day}` : day.toString();
+  return path.resolve(__dirname, `../src/days/day_${dayNumber}/${fileName}`);
 };
 
 const config = {
