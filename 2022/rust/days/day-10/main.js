542704f38d46da71d6dc16714e9a2c24d3ffcbbc
Bruno Kawka <brunokawka@gmail.com>
2022-12-12 08:53:48 +0100

    Clean up day 10 of year 2022

--- 2022/rust/days/day-10/main.js
+++ 2022/rust/days/day-10/main.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 const CRT_WIDTH = 40;
-const CRT_HEIGHT = 6;
 const SPRITE_RADIUS = 2;
 
 const parse = (source) =>
@@ -12,153 +11,7 @@ const parse = (source) =>
     .map(([instr, value]) => [instr, value && Number(value)]);
 
 const data = parse(fs.readFileSync('in.txt', 'utf-8'));
-
-const test = parse(`addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop`);
+const test = parse(fs.readFileSync('test.txt', 'utf-8'));
 
 class CPU {
   constructor() {
@@ -194,29 +47,33 @@ class CPU {
   }
 }
 
-const part1 = (data) => {
-  const cpu = new CPU();
+const getPixelSymbol = (cycleValue, pixelIndex) => {
+  return Math.abs(cycleValue - (pixelIndex % CRT_WIDTH)) < SPRITE_RADIUS
+    ? '⬜️'
+    : '⬛️';
+};
 
-  data.forEach(([command, value]) => cpu.runTask(command, value));
-
+const part1 = (cpu) => {
   return [20, 60, 100, 140, 180, 220]
     .map((cycleIndex) => cycleIndex * cpu.getNthCycleValue(cycleIndex))
     .reduce((a, b) => a + b);
 };
 
-const part2 = (data) => {
-  const cpu = new CPU();
-
-  data.forEach(([command, value]) => cpu.runTask(command, value));
+const part2 = (cpu) => {
   return cpu
     .getAllCycleValues()
-    .map((value, i) => {
-      const sym = Math.abs(value - (i % CRT_WIDTH)) < SPRITE_RADIUS ? '⚪️' : '⚫️';
-      const newline = i && i % CRT_WIDTH === 0;
-      return newline ? `\n${sym}` : sym;
-    })
+    .map(getPixelSymbol)
+    .map((symbol, i) => (i > 0 && i % CRT_WIDTH === 0 ? `\n${symbol}` : symbol))
     .join('');
 };
 
-console.log(part1(test));
-console.log(part2(data));
+const run = (data) => {
+  const cpu = new CPU();
+  data.forEach(([command, value]) => cpu.runTask(command, value));
+
+  console.log(part1(cpu));
+  console.log(part2(cpu));
+};
+
+run(test);
+run(data);
