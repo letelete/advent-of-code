c7d01aafbd6436f9fbc5453697770ba143f0ce60
letelete <brunokawka@gmail.com>
2024-12-04 09:28:15 +0100

    Add day 4 of year 2024 👼

--- 2024/days/day-04/main.js
+++ 2024/days/day-04/main.js
@@ -6,12 +6,22 @@ const data = parse(fs.readFileSync("in.txt", "utf-8"));
 const testOne = parse(fs.readFileSync("test.one.txt", "utf-8"));
 const testTwo = parse(fs.readFileSync("test.two.txt", "utf-8"));
 
-Array.prototype.sum = function () {
-  return this.reduce((sum, value) => sum + value, 0);
-};
-
-Array.prototype.product = function () {
-  return this.reduce((x, value) => x * value, 1);
+const dirs = {
+  orthogonal: [
+    [0, -1],
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+  ],
+  diagonal: [
+    [-1, -1],
+    [-1, 1],
+    [1, 1],
+    [1, -1],
+  ],
+  all() {
+    return [...this.orthogonal, ...this.diagonal];
+  },
 };
 
 function* createGridTraversal(grid, [initialRow, initialCol] = [0, 0]) {
@@ -22,21 +32,9 @@ function* createGridTraversal(grid, [initialRow, initialCol] = [0, 0]) {
   }
 }
 
-const dirsLTRB = [
-  [0, -1],
-  [-1, 0],
-  [0, 1],
-  [1, 0],
-];
+const getGridValue = (grid, [row, col]) => grid[row][col];
 
-const dirsLTRBDiag = [
-  [-1, -1],
-  [-1, 1],
-  [1, 1],
-  [1, -1],
-];
-
-const allDirs = [...dirsLTRB, ...dirsLTRBDiag];
+const reflection = ([x, y]) => [x * -1, y * -1];
 
 function Pos(initialRow, initialCol) {
   let row = initialRow;
@@ -59,88 +57,57 @@ function Pos(initialRow, initialCol) {
   };
 }
 
-function reflection([x, y]) {
-  return [x * -1, y * -1];
-}
-
-function getGridValue(grid, [row, col]) {
-  return grid[row][col];
-}
-
-function checkForPatternInDir(grid, [row, col], [dirRow, dirCol], patternQ) {
+function matchesPattern(grid, [row, col], [dirRow, dirCol], pattern) {
   const pos = Pos(row, col);
 
-  while (
-    patternQ.length &&
-    pos.inRange(0, grid.length - 1, 0, grid[0].length - 1)
-  ) {
-    if (getGridValue(grid, pos.current) === patternQ[0]) {
-      patternQ.shift();
-    } else {
-      break;
+  for (const next of pattern) {
+    if (
+      !pos.inRange(0, grid.length - 1, 0, grid[0].length - 1) ||
+      getGridValue(grid, pos.current) !== next
+    ) {
+      return false;
     }
-
     pos.incr([dirRow, dirCol]);
   }
 
-  return patternQ.length === 0;
+  return true;
 }
 
 const countXmasWords = (grid, [row, col]) => {
-  let count = 0;
-
-  for (const dir of allDirs) {
-    if (checkForPatternInDir(grid, [row, col], dir, "XMAS".split(""))) {
-      count++;
-    }
-  }
-
-  return count;
+  return dirs
+    .all()
+    .filter((dir) => matchesPattern(grid, [row, col], dir, "XMAS".split("")))
+    .length;
 };
 
 const hasShapeOfXmas = (grid, [row, col]) => {
   const pos = Pos(row, col);
 
-  if (getGridValue(grid, pos.current) !== "A") {
-    return false;
-  }
-
-  let count = 0;
-  for (const dir of dirsLTRBDiag) {
-    if (
-      checkForPatternInDir(grid, pos.toIncr(dir).current, dir, ["M"]) &&
-      checkForPatternInDir(grid, pos.toIncr(reflection(dir)).current, dir, [
-        "S",
-      ])
-    ) {
-      count++;
-    }
-  }
-
-  return count === 2;
+  return (
+    getGridValue(grid, pos.current) === "A" &&
+    dirs.diagonal.filter(
+      (dir) =>
+        matchesPattern(grid, pos.toIncr(dir).current, dir, ["M"]) &&
+        matchesPattern(grid, pos.toIncr(reflection(dir)).current, dir, ["S"])
+    ).length === 2
+  );
 };
 
 const partOne = (data) => {
-  const dataTraversal = createGridTraversal(data);
   let count = 0;
-
-  for (const pos of dataTraversal) {
+  for (const pos of createGridTraversal(data)) {
     count += countXmasWords(data, pos);
   }
-
   return count;
 };
 
 const partTwo = (data) => {
-  const dataTraversal = createGridTraversal(data);
   let count = 0;
-
-  for (const pos of dataTraversal) {
+  for (const pos of createGridTraversal(data)) {
     if (hasShapeOfXmas(data, pos)) {
       count++;
     }
   }
-
   return count;
 };
 
