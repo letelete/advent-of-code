76d7cfc7d8f0fafa3ab61cdc1e4663dc6b0d86f6
letelete <brunokawka@gmail.com>
2024-12-05 18:21:54 +0100

    Add day 5 of year 2024 🦌

--- 2024/days/day-05/main.js
+++ 2024/days/day-05/main.js
@@ -14,7 +14,9 @@ Array.prototype.sum = function () {
 };
 
 Array.prototype.equals = function (arr) {
-  return this.every((e, i) => e === arr[i]);
+  return (
+    this.length === arr.length && this.every((e, i) => Object.is(e, arr[i]))
+  );
 };
 
 const compare = (rules) => (a, b) => {
@@ -25,29 +27,63 @@ const compare = (rules) => (a, b) => {
   return rule[0] === a ? -1 : 1;
 };
 
-const partOne = (data) => {
+function part1(data) {
+  const sortCompare = compare(data.rules);
   return data.updates
-    .map((update) => update.toSorted(compare(data.rules)))
+    .map((update) => update.toSorted(sortCompare))
     .filter((update, index) => update.equals(data.updates[index]))
     .map((update) => update[Math.floor(update.length / 2)])
     .sum();
-};
+}
 
-const partTwo = (data) => {
+function part2(data) {
+  const sortCompare = compare(data.rules);
   return data.updates
-    .map((update) => update.toSorted(compare(data.rules)))
+    .map((update) => update.toSorted(sortCompare))
     .filter((update, index) => !update.equals(data.updates[index]))
     .map((update) => update[Math.floor(update.length / 2)])
     .sum();
-};
+}
 
-const data = parse(fs.readFileSync("in.txt", "utf-8"));
-const testOne = parse(fs.readFileSync("test.one.txt", "utf-8"));
-const testTwo = parse(fs.readFileSync("test.two.txt", "utf-8"));
+/* -------------------------------------------------------------------------------------------------
+ * Read Input & Print Output boilerplate
+ * -----------------------------------------------------------------------------------------------*/
 
-console.log("\x1b[31m--- DATA---\x1b[0m\n", data);
-console.log("\x1b[31m--- TEST PART ONE ---\x1b[0m\n", partOne(testOne));
-console.log("\x1b[31m--- TEST PART TWO ---\x1b[0m\n", partTwo(testTwo));
-console.log();
-console.log("\x1b[31m--- PART ONE ---\x1b[0m\n", partOne(data));
-console.log("\x1b[31m--- PART TWO ---\x1b[0m\n", partTwo(data));
+function withInput(callback) {
+  const [data, ...samples] = fs
+    .readdirSync(".", { withFileTypes: true })
+    .filter((e) => e.isFile() && e.name.match(/^in(\.sample(.*)?)?\.txt$/))
+    .sort((file) => (Boolean(file.name.match(/\.sample/)) ? 1 : -1))
+    .map((file) => parse(fs.readFileSync(file.name, "utf-8")));
+
+  return callback(data, samples);
+}
+
+withInput((data, samples) => {
+  const color = {
+    primary: "\x1b[97m",
+    secondary: "\x1b[90m",
+    accent: "\x1b[35m",
+    reset: "\x1b[0m",
+  };
+  const component = {
+    heading: (str, colorCode = color.primary) =>
+      `${colorCode}=== ${str.toUpperCase()} ===${color.reset}`,
+    subheading: (str, colorCode = color.secondary) =>
+      `${colorCode}--- ${str.toUpperCase()} ---${color.reset}`,
+  };
+  const print = {
+    section: (heading, data, ans1, ans2) => {
+      console.log(component.heading(heading, color.accent));
+      console.dir(data, { maxArrayLength: 5, depth: null });
+      console.log(component.subheading("part 1"));
+      console.log(ans1);
+      console.log(component.subheading("part 2"));
+      console.log(ans2);
+    },
+  };
+  samples.forEach((sample, index) =>
+    print.section(`sample ${index + 1}`, sample, part1(sample), part2(sample))
+  );
+  print.section(`answer`, data, part1(data), part2(data));
+});
