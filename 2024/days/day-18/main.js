c1ee0574e5c4b9502b02f55c7e258ab8a1a2ed73
letelete <brunokawka@gmail.com>
2024-12-18 09:27:40 +0100

    Add day 18 of year 2024 🥛

---
+++ 2024/days/day-18/main.js
@@ -0,0 +1,27 @@
+function parse(source) {
+  return source.trim().split('\n');
+}
+
+Array.prototype.sum = function () {
+  return this.reduce((sum, value) => sum + value, 0);
+};
+
+Array.prototype.product = function () {
+  return this.reduce((x, value) => x * value, 1);
+};
+
+Array.prototype.equals = function (arr) {
+  return (
+    this.length === arr.length && this.every((e, i) => Object.is(e, arr[i]))
+  );
+};
+
+function part1(data) {
+  return null;
+}
+
+function part2(data) {
+  return null;
+}
+
+module.exports = { parse, part1, part2 };
