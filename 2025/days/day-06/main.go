d85a2a4ff9a48fd5e3a0b6d7af2041365b232e43
letelete <brunokawka@gmail.com>
2025-12-06 23:15:59 +0100

    Add day 06 of year 2025 🎅

--- 2025/days/day-06/main.go
+++ 2025/days/day-06/main.go
@@ -3,22 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 )
-
-func arrMax(arr []int) int {
-	max := arr[0]
-	for _, x := range arr {
-		if x > max {
-			max = x
-		}
-	}
-	return max
-}
 
 type Expr struct {
 	Numbers []int
@@ -44,102 +33,45 @@ func (e *Expr) Eval() int {
 	return out
 }
 
-func partition(num, pad int) []int {
-	cap := int(math.Floor(math.Log10(float64(num)))) + 1
-	p := make([]int, pad)
-	for i := range p {
-		p[i] = -1
-	}
-	for i := range cap {
-		p[pad-i-1] = num % 10
-		num /= 10
-	}
-	return p
-}
-
-func (e *Expr) TransformNumbers() {
-	pad := int(math.Floor(math.Log10(float64(arrMax(e.Numbers))))) + 1
-	partitions := make([][]int, 0, len(e.Numbers))
-	for _, x := range e.Numbers {
-		partitions = append(partitions, partition(x, pad))
-	}
-	transformed := make([]int, 0, pad)
-	for i := range pad {
-		num := 0
-		factor := 0
-		for _, p := range partitions {
-			if p[i] != -1 {
-				if factor == 0 {
-					factor = 1
-				} else {
-					factor *= 10
-				}
-			}
-		}
-		if factor == 0 {
-			continue
-		}
-		for _, p := range partitions {
-			if p[i] != -1 {
-				num += p[i] * factor
-				factor /= 10
-			}
-		}
-		transformed = append(transformed, num)
-	}
-	e.Numbers = transformed
-}
-
-func main() {
-	testData := readFile("test.txt")
-	for _, x := range testData {
-		fmt.Println("before", x.Numbers)
-		x.TransformNumbers()
-		fmt.Println("after", x.Numbers)
-	}
-	fmt.Println("[test case] part 1:", part1(testData), ", expected: ", 4277556)
-	fmt.Println("[test case] part 2:", part2(testData), ", expected: ", 0)
-	data := readFile("in.txt")
-	fmt.Println("part 1:", part1(data))
-	fmt.Println("part 2:", part2(data))
-}
-
-func part1(data []Expr) int {
+func sumExpr(expr []Expr) int {
 	sum := 0
-	for _, e := range data {
+	for _, e := range expr {
 		sum += e.Eval()
 	}
 	return sum
 }
 
-func part2(data []Expr) int {
-	sum := 0
-	return sum
+func toReversed(arr []string) []string {
+	a := make([]string, len(arr))
+	copy(a, arr)
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+	return a
 }
 
-func readFile(filename string) []Expr {
-	path, err := filepath.Abs(filename)
-	if err != nil {
-		panic(fmt.Sprintf("cannot get absolute path for %q: %v", filename, err))
-	}
-	f, err := os.Open(path)
-	if err != nil {
-		panic(fmt.Sprintf("cannot open file %q: %v", path, err))
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			panic(fmt.Sprintf("cannot close file %q: %v", path, err))
-		}
-	}()
+func main() {
+	fmt.Println("[test case] part 1:", part1(readFile("test.txt", parsePart1)), ", expected: ", 4277556)
+	fmt.Println("[test case] part 2:", part2(readFile("test.txt", parsePart2)), ", expected: ", 3263827)
+	fmt.Println("part 1:", part1(readFile("in.txt", parsePart1)))
+	fmt.Println("part 2:", part2(readFile("in.txt", parsePart2)))
+}
 
+func part1(data []Expr) int {
+	return sumExpr(data)
+}
+
+func part2(data []Expr) int {
+	return sumExpr(data)
+}
+
+func parsePart1(s *bufio.Scanner) []Expr {
 	out := []Expr{}
 	numbers := [][]int{}
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
+	for s.Scan() {
 		row := []int{}
-		tokenI := 0
-		for token := range strings.SplitSeq(scanner.Text(), " ") {
+		tokenIndex := 0
+		for token := range strings.SplitSeq(s.Text(), " ") {
 			if len(strings.TrimSpace(token)) == 0 {
 				continue
 			}
@@ -147,21 +79,98 @@ func readFile(filename string) []Expr {
 			if err == nil {
 				row = append(row, num)
 			} else {
-				// symbols line
 				expr := Expr{make([]int, 0, len(numbers)), token[0]}
 				for i := range numbers {
-					expr.Numbers = append(expr.Numbers, numbers[i][tokenI])
+					expr.Numbers = append(expr.Numbers, numbers[i][tokenIndex])
 				}
 				out = append(out, expr)
 			}
-			tokenI++
+			tokenIndex++
 		}
 		if len(row) > 0 {
 			numbers = append(numbers, row)
 		}
 	}
-	if err := scanner.Err(); err != nil {
+	return out
+}
+
+func parsePart2(s *bufio.Scanner) []Expr {
+	getOffset := func(start int, line string) int {
+		offset := 1
+		for (start+offset < len(line) && line[start+offset] == ' ') || (start+offset == len(line)) {
+			offset++
+		}
+
+		return offset
+	}
+
+	raw := []string{}
+	for s.Scan() {
+		raw = append(raw, s.Text())
+	}
+
+	out := []Expr{}
+
+	start := []int{}
+	end := []int{}
+	symbolsLine := raw[len(raw)-1]
+	numsLines := raw[:len(raw)-1]
+
+	for i := 0; i < len(symbolsLine); i++ {
+		if symbolsLine[i] != ' ' {
+			offset := getOffset(i, symbolsLine)
+			if offset < 1 {
+				panic(fmt.Errorf("invalid state: offset '%v' < 1 for line '%v' at %v", offset, symbolsLine, i))
+			}
+			start = append(start, i)
+			end = append(end, i+offset)
+			i += offset - 1
+		}
+	}
+
+	for i := range start {
+		_start := start[i]
+		_end := end[i]
+		pad := _end - _start - 1
+		transformed := make([]int, 0, pad)
+		for padIndex := range pad {
+			num := 0
+			factor := 1
+			for _, line := range toReversed(numsLines) {
+				entry := line[_start : _end-1]
+				if entry[padIndex] != ' ' {
+					num += int(entry[padIndex]-'0') * factor
+					factor *= 10
+				}
+			}
+			transformed = append(transformed, num)
+		}
+		out = append(out, Expr{transformed, symbolsLine[start[i]]})
+	}
+
+	return out
+}
+
+func readFile(filename string, parse func(s *bufio.Scanner) []Expr) []Expr {
+	path, err := filepath.Abs(filename)
+	if err != nil {
+		panic(fmt.Sprintf("cannot get absolute path for %q: %v", filename, err))
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(fmt.Sprintf("cannot open file %q: %v", path, err))
+	}
+
+	s := bufio.NewScanner(f)
+	out := parse(s)
+
+	if err := s.Err(); err != nil {
 		panic(fmt.Sprintf("error reading file %q: %v", path, err))
 	}
+	if err := f.Close(); err != nil {
+		panic(fmt.Sprintf("cannot close file %q: %v", path, err))
+	}
+
 	return out
 }
